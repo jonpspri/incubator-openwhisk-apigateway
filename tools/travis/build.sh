d1658135328690d078bc56534ef4bd6050da57ac
Jonathan Springer <jonpspri@gmail.com>
2018-03-30 13:29:03 +0000

    Build updates

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -21,16 +21,16 @@ set -x
 
 # Build script for Travis-CI.
 SCRIPTDIR="$(cd "$(dirname "$0")" && pwd)"
-ROOTDIR="$SCRIPTDIR/../.."
-HOMEDIR="$ROOTDIR/.."
-WHISKDIR="$ROOTDIR/../openwhisk"
+ROOTDIR=$(cd "$SCRIPTDIR/../.." && pwd)
+HOMEDIR=$(cd "$ROOTDIR/.." && pwd)
+WHISKDIR=$(cd "$ROOTDIR/../openwhisk" && pwd)
 
 # run the scancode util. against project source code starting at its root
-cd $HOMEDIR
-incubator-openwhisk-utilities/scancode/scanCode.py $ROOTDIR --config $ROOTDIR/tools/build/scanCode.cfg
+cd "$HOMEDIR"
+incubator-openwhisk-utilities/scancode/scanCode.py "$ROOTDIR" --config "$ROOTDIR/tools/build/scanCode.cfg"
 
 # Install OpenWhisk
-cd $WHISKDIR/ansible
+cd "$WHISKDIR/ansible"
 
 ANSIBLE_CMD="ansible-playbook -i environments/local  -e docker_image_prefix=openwhisk"
 
@@ -39,10 +39,10 @@ $ANSIBLE_CMD prereq.yml
 $ANSIBLE_CMD couchdb.yml
 $ANSIBLE_CMD initdb.yml
 
-# build docker image locally
-pushd $ROOTDIR
+# build docker image
+pushd "$ROOTDIR"
 pwd
-docker build . -t "openwhisk/apigateway"
+./gradlew --console=plain :core:apigateway:dockerBuildImage
 popd
 
 #Use local
@@ -51,7 +51,6 @@ $ANSIBLE_CMD apigateway.yml -e apigateway_local_build=true
 #Use dockerhub
 #$ANSIBLE_CMD apigateway.yml
 
-
 $ANSIBLE_CMD wipe.yml
 $ANSIBLE_CMD openwhisk.yml -e cli_installation_mode=remote -e controllerProtocolForSetup=http
 
@@ -59,13 +58,13 @@ $ANSIBLE_CMD openwhisk.yml -e cli_installation_mode=remote -e controllerProtocol
 export OPENWHISK_HOME=$WHISKDIR
 
 # Tests
-cd $WHISKDIR
+cd "$WHISKDIR"
 cat whisk.properties
 
-WSK_TESTS_DEPS_EXCLUDE="-x :actionRuntimes:pythonAction:distDocker -x :actionRuntimes:javaAction:distDocker -x :actionRuntimes:nodejs6Action:distDocker -x :actionRuntimes:nodejs8Action:distDocker -x :actionRuntimes:actionProxy:distDocker -x :sdk:docker:distDocker -x :actionRuntimes:python2Action:distDocker -x :tests:dat:blackbox:badaction:distDocker -x :tests:dat:blackbox:badproxy:distDocker"
+WSK_TESTS_DEPS_EXCLUDE=( -x :actionRuntimes:pythonAction:distDocker -x :actionRuntimes:javaAction:distDocker -x :actionRuntimes:nodejs6Action:distDocker -x :actionRuntimes:nodejs8Action:distDocker -x :actionRuntimes:actionProxy:distDocker -x :sdk:docker:distDocker -x :actionRuntimes:python2Action:distDocker -x :tests:dat:blackbox:badaction:distDocker -x :tests:dat:blackbox:badproxy:distDocker )
 
-TERM=dumb ./gradlew tests:test --tests apigw.healthtests.* ${WSK_TESTS_DEPS_EXCLUDE}
-sleep 60
-TERM=dumb ./gradlew tests:test --tests whisk.core.apigw.* ${WSK_TESTS_DEPS_EXCLUDE}
-sleep 60
-TERM=dumb ./gradlew tests:test --tests whisk.core.cli.test.ApiGwRestTests ${WSK_TESTS_DEPS_EXCLUDE}
+./gradlew --console=plain :tests:test --tests "apigw.healthtests.*" "${WSK_TESTS_DEPS_EXCLUDE[@]}"
+echo 'Sleeping 60... never fear'; sleep 60
+./gradlew --console=plain :tests:test --tests "whisk.core.apigw.*" "${WSK_TESTS_DEPS_EXCLUDE[@]}"
+echo 'Sleeping 60... never fear'; sleep 60
+./gradlew --console=plain :tests:test --tests "whisk.core.cli.test.ApiGwRestTests" "${WSK_TESTS_DEPS_EXCLUDE[@]}"
